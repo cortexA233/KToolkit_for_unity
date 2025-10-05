0f2824be59dd52d531d6326271a0c21f0e4f5e94
cortexA233 <cortexA233@outlook.com>
2025-10-05 01:34:06 -0400

    test update

--- Framework/StateMachineLib.cs
+++ Framework/StateMachineLib.cs
@@ -32,6 +32,35 @@ public abstract class BaseFSM : KObserverNoMono
 }
 
 
+public abstract class KBaseState<TOwner> where TOwner : MonoBehaviour
+{
+    public TOwner owner;
+    public KBaseState(TOwner owner)
+    {
+        this.owner = owner;
+    }
+    public abstract void EnterState();
+    public abstract void HandleFixedUpdate();
+    public abstract void HandleUpdate();
+    public abstract void ExitState();
+    public abstract void HandleCollide2D(Collision2D collision);
+    public abstract void HandleTrigger2D(Collider2D collider);
+}
+
+
+public abstract class KBaseFSM<TOwner> : KObserverNoMono where TOwner : MonoBehaviour
+{
+    public TOwner owner { protected set; get; }
+    public KBaseState<TOwner> currentState { protected set; get; }
+    public void TransitState(KBaseState<TOwner> newState)
+    {
+        currentState.ExitState();
+        currentState = null;
+        currentState = newState;
+    }
+}
+
+
 // public class StateMachineEventKObserver : KObserverNoMono
 // {
 //     public 
