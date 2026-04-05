6927f5f83c9e07f22253ac34fc190d21735a1e47
cortexA233 <cortexA233@outlook.com>
2026-04-05 00:39:11 -0400

    update new default scene config

--- Framework/KFrameworkManager.cs
+++ Framework/KFrameworkManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
 namespace KToolkit
@@ -18,11 +20,46 @@ namespace KToolkit
         public virtual void InitKFramework()
         {
             // KTickManager.instance
-            DontDestroyOnLoad(GameObject.Find("KCanvas"));
-            DontDestroyOnLoad(GameObject.Find("Main Camera"));
-            DontDestroyOnLoad(GameObject.Find("EventSystem"));;
-            DontDestroyOnLoad(GameObject.Find("pool_transform_parent"));
+            var canvasObject = GetOrCreateKCanvas();
+            var eventSystemObject = GetOrCreateEventSystem();
+
+            DontDestroyOnLoad(canvasObject);
+            DontDestroyOnLoad(eventSystemObject);
+            // DontDestroyOnLoad(GameObject.Find("pool_transform_parent"));
             KUIManager.instance.Init();
+        }
+
+        private static GameObject GetOrCreateKCanvas()
+        {
+            var canvasObject = GameObject.Find("KCanvas");
+            if (canvasObject != null)
+            {
+                return canvasObject;
+            }
+
+            canvasObject = new GameObject("KCanvas");
+            canvasObject.AddComponent<Canvas>();
+            var canvasScaler = canvasObject.AddComponent<CanvasScaler>();
+            canvasObject.AddComponent<GraphicRaycaster>();
+
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(1920f, 1080f);
+
+            return canvasObject;
+        }
+
+        private static GameObject GetOrCreateEventSystem()
+        {
+            var eventSystemObject = GameObject.Find("EventSystem");
+            if (eventSystemObject != null)
+            {
+                return eventSystemObject;
+            }
+
+            eventSystemObject = new GameObject("EventSystem");
+            eventSystemObject.AddComponent<EventSystem>();
+            eventSystemObject.AddComponent<StandaloneInputModule>();
+            return eventSystemObject;
         }
 
         private void Update()
